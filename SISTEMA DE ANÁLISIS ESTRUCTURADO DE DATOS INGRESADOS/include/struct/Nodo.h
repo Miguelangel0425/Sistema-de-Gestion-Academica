83e2c5ab84d2b824d60c27aed1daacc55e291e94
wikigogeta2005 <kenaiwiki@gmail.com>
2026-02-02 21:41:35 -0500

    Update Nodo.h

--- SISTEMA DE ANÁLISIS ESTRUCTURADO DE DATOS INGRESADOS/include/struct/Nodo.h
+++ SISTEMA DE ANÁLISIS ESTRUCTURADO DE DATOS INGRESADOS/include/struct/Nodo.h
@@ -1,6 +1,26 @@
 #ifndef NODO_H
 #define NODO_H
 
+template <typename T>
+// Nodo para lista doblemente enlazada
+
+struct nodoArbol {
+	T dato;
+	nodoArbol<T> *izq;
+	nodoArbol<T> *der;
+
+    nodoArbol(T _dato) : dato(_dato),izq(nullptr),der(nullptr) {};
+};
+
+template <typename T>
+
+struct nodoSimple{
+    T dato;
+    nodoSimple<T> *siguiente;
+
+    nodoSimple(T _dato): dato(_dato), siguiente(nullptr){};
+};
+
 
 
 #endif NODO_H
